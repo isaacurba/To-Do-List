896e9a29d3dea83edc21e7c51f46ff64a8433205
isaacurba <kpomassiurban@gmail.com>
2025-07-15 20:07:46 +0100

    Added header foter and about EJS template Layouts
    added header footer and about EJS

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const __dirname = path.dirname(__filename);
 app.set("view engine", "ejs"); 
 
     let newItems = [];
+    let workItems = [];
 
 
 app.get("/", (req, res) => {
@@ -24,20 +25,50 @@ app.get("/", (req, res) => {
         year: 'numeric',
         month: 'long',
     };
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
         
-    res.render("list", { kindOfDay: day, newListItems: newItems });
-    });
+    res.render("list", { listTitle: day, newListItems: newItems });
+});
 
 app.post("/", (req, res) => {
-    let newItem = req.body.newItem; 
-    newItems.push(newItem); 
-    res.redirect("/");
+    let newItem = req.body.newItem;
+    
+    if (req.body.list === "Work List") {
+        workItems.push(newItem);   
+        res.redirect("work")
+    }else{
+        newItems.push(newItem); 
+        res.redirect("/");
+    }
+
+    console.log(req.body);
+});
+
+
+// FOR WORK ROUTE
+
+
+app.get("/work", (req, res)=>{
+    res.render("list", {listTitle: "Work List", newListItems: workItems})
+});
+
+app.post("/work", (req, res)=>{
+    let workItem = req.body.newItem;
+    workItems.push(workItem);
+    res.redirect("/work");
+});
+
+// about route
+
+app.get("/about", (req, res)=>{
+    res.render("about");
 });
 
 
 app.listen(3000, () => {
     console.log("Server responding at port 3000!!");
 });
+
+
 
 
